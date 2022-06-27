b0cf08cefb8118a7cabb0e07dbae0b32c4fa7ebf
imliterallydanishlol <102416852+bambitheone82112@users.noreply.github.com>
2022-06-27 09:50:59 +0700

    Create Playstate.hx

---
+++ Source/Playstate.hx
@@ -0,0 +1,24 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+    override public function create()
+    {
+        super.create();
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
+}
+override public function create()
+{
+    super.create();
+
+    var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+    text.screenCenter();
+    add(text);
+}
