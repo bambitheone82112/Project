d160f9d8c43eee2fdda9301efb187d0edc35b62b
imliterallydanishlol <102416852+bambitheone82112@users.noreply.github.com>
2022-06-27 09:58:24 +0700

    Menu

--- Source/MenuState.hx
+++ Source/MenuState.hx
@@ -1,9 +1,11 @@
 package;
 
 import flixel.FlxState;
+import flixel.ui.FlxButton;
+import flixel.FlxG;
 
- class MenuState extends FlxState
- {
+class MenuState extends FlxState
+{
     override public function create()
     {
         super.create();
@@ -16,9 +18,18 @@ import flixel.FlxState;
 }
 override public function create()
 {
+    playButton.screenCenter();
     super.create();
 
     var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
     text.screenCenter();
     add(text);
 }
+var playButton:FlxButton;
+ playButton = new FlxButton(0, 0, "Play", clickPlay);
+ add(playButton);
+ function clickPlay()
+ {
+     FlxG.switchState(new PlayState());
+ }
+
